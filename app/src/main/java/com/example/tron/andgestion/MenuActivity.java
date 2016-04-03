b80efb3f208f5ef55fc54f057a2b058ff99bcf0a
ronny0005 <ronald2000fr@gmail.com>
2016-04-03 10:41:46 +0200

    maj

--- app/src/main/java/com/example/tron/andgestion/MenuActivity.java
+++ app/src/main/java/com/example/tron/andgestion/MenuActivity.java
@@ -3,8 +3,10 @@ package com.example.tron.andgestion;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.tron.andgestion.bddlocal.article.ArticleServeur;
 import com.example.tron.andgestion.bddlocal.client.Client;
@@ -23,6 +25,7 @@ public class MenuActivity extends AppCompatActivity {
     Button parametre;
     Button etat;
     outils ou;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
